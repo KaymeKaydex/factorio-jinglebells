01339d18b4344365497be77f7093b3c1f0224a15
staurran <staurran@gmail.com>
2024-12-28 20:45:36 +0300

    fix christmas tree final

--- data.lua
+++ data.lua
@@ -1,13 +1,13 @@
 data:extend { {
-    type = "terrain",
+    type = "impact-category",
     name = "christmas-tree",
 } }
 
 do
-    local item = table.deepcopy(data.raw.terrain['trees'])
+    local item = table.deepcopy(data.raw.tree["tree-01"])
     item.name = "christmas-tree"
     item.order = "c[christmas-tree]"
     item.place_result = "christmas-tree"
-    item.icon = "__jinglebells__/graphics/icons/spoiling-plant.png"
+    item.icon = "__jinglebells__/graphics/icons/christmas_tree.png"
     data:extend { item }
 end
